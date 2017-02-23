787f8a8e91bd482df93d94b037a7d5768b1a458c
Jack Lu <jacklu@jacklu.me>
2017-02-23 18:24:21 +0800

    +

---
+++ angular-cssx.js
@@ -0,0 +1,26 @@
+﻿angular.module("angular-cssx", [])
+	.directive('cssx', function (cssxConfig) {
+		return {
+			compile: function (element, attr) {
+				var text = document.getElementById(attr.cssx).innerHTML;
+				var elementrule = cssx(cssx.parse(text))(element);
+				for (var id in elementrule) {
+					var element = elementrule[id].element;
+					var style = elementrule[id].style;
+					for (var name in style) {
+						var applier = cssxConfig.applier[name];
+						applier.call(angular.element(element), style[name]);
+					}
+				}
+			}
+		};
+	})
+	.provider('cssxConfig', function () {
+		return {
+			applier: {},
+			$get: function () { return this; }
+		};
+	})
+	.config(function (cssxConfigProvider) {
+		cssxConfigProvider.applier.before = function (value) { this.before(value); };
+	})
